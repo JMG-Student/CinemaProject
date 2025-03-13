379992e8ea92f453b213a100968d12e02e59760e
JMG-Student <L00175049@atu.ie>
2025-03-13 16:24:04 +0000

    Fixed Redirect

--- CinemaProject/Pages/Customer/Bookings/Confirmation.cshtml.cs
+++ CinemaProject/Pages/Customer/Bookings/Confirmation.cshtml.cs
@@ -1,12 +1,39 @@
+using CinemaProject.Models.Models;
+using CinemaProject.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using CinemaProject.Models.Models;
 
 namespace CinemaProject.Pages.Customer.Bookings
 {
     public class ConfirmationModel : PageModel
     {
-        public void OnGet()
-        {
-        }
-    }
+			private readonly IUnitOfWork _unitOfWork;
+
+			public ConfirmationModel(IUnitOfWork unitOfWork)
+			{
+				_unitOfWork = unitOfWork;
+			}
+
+			public Booking booking { get; set; }
+
+			public void OnGet(int id)
+			{
+
+				booking = _unitOfWork.BookingRepo.Get(id);
+				
+				foreach (var ticket in _unitOfWork.TicketRepo.GetAll())
+				{
+					if (ticket.BookingId == booking.Id)
+					{
+						ticket.TicketType = _unitOfWork.TicketTypeRepo.Get(ticket.TicketTypeId);
+						ticket.Screening = _unitOfWork.ScreeningRepo.Get(ticket.ScreeningId);
+						booking.Tickets.Add(ticket);
+					}
+				
+				}
+
+		}
+
+	}
 }
