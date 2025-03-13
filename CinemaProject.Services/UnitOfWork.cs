162018641ac65024e2c7e86dbdc8834f21fc4bfb
Aoife Doherty <157698571+ACD31@users.noreply.github.com>
2025-03-13 14:56:44 +0000

    added all old code into new branch and it works

--- CinemaProject.Services/UnitOfWork.cs
+++ CinemaProject.Services/UnitOfWork.cs
@@ -15,25 +15,31 @@ namespace CinemaProject.Services
 
         public IFilmRepo FilmRepo { get; private set; }
         public IGenreRepo GenreRepo { get; private set; }
-
-        public ITicketRepo TicektRepo { get; private set; }
-		public ITicketTypeRepo TicketTypeRepo { get; private set; }
+        public ITicketRepo TicketRepo { get; private set; }
+        public ITicketTypeRepo TicketTypeRepo { get; private set; }
 		public IBookingRepo BookingRepo { get; private set; }
 		public IScreeningRepo ScreeningRepo { get; private set; }
+        public ICapacityRepo CapacityRepo { get; private set; }
+        public IScreenRepo ScreenRepo { get; private set; }
 
 
-		public UnitOfWork(AppDBContext appDBContext)
+
+
+        public UnitOfWork(AppDBContext appDBContext)
         {
 
             _dbContext = appDBContext;
             FilmRepo = new FilmRepo(_dbContext);
             GenreRepo = new GenreRepo(_dbContext);
-			TicektRepo = new TicketRepo(_dbContext);
-			TicketTypeRepo = new TicketTypeRepo(_dbContext);
+            TicketRepo = new TicketRepo(_dbContext); 
+            TicketTypeRepo = new TicketTypeRepo(_dbContext);
 			BookingRepo = new BookingRepo(_dbContext);
 			ScreeningRepo = new ScreeningRepo(_dbContext);
+            ScreenRepo = new ScreenRepo(_dbContext);
+            CapacityRepo = new CapacityRepo(_dbContext);
 
-		}
+
+        }
 
         public void Dispose()
         {
---
+++ CinemaProject/Pages/Admin/Capacities/Delete.cshtml.cs
@@ -0,0 +1,47 @@
+using CinemaProject.Models.Models;
+using CinemaProject.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CinemaProject.Pages.Admin.Capacities
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeleteModel(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public Cap Cap { get; set; } 
+     
+        public void OnGet(int id)
+        {
+            Cap = _unitOfWork.CapacityRepo.Get(id);
+        }
+
+        public IActionResult OnPost(int id)
+        {
+            if (id <= 0)
+            {
+                ModelState.AddModelError("", "Invalid ID provided.");
+                return Page();
+            }
+
+            var capToDelete = _unitOfWork.CapacityRepo.Get(id);
+
+            if (capToDelete == null)
+            {
+                ModelState.AddModelError("", $"Error: No capacity found with ID {id}.");
+                return Page();
+            }
+
+            _unitOfWork.CapacityRepo.Delete(capToDelete);
+            _unitOfWork.Save();
+
+            return RedirectToPage("Index");
+        }
+
+    }
+}
