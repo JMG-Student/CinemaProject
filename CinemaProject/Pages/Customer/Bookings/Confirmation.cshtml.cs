87e9eb4dbabc718b12febe57cf8258afba0fec0a
JMG-Student <L00175049@atu.ie>
2025-04-10 16:10:45 +0100

    Ficed Confimration

--- CinemaProject/Pages/Customer/Bookings/Confirmation.cshtml.cs
+++ CinemaProject/Pages/Customer/Bookings/Confirmation.cshtml.cs
@@ -2,39 +2,60 @@ using CinemaProject.Models.Models;
 using CinemaProject.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using QRCoder;
+using CinemaProject.Pages.PageViewModels;
+using System.Drawing;
 
 
 namespace CinemaProject.Pages.Customer.Bookings
 {
     public class ConfirmationModel : PageModel
     {
-			private readonly IUnitOfWork _unitOfWork;
+		private readonly IUnitOfWork _unitOfWork;
 
-			public ConfirmationModel(IUnitOfWork unitOfWork)
+		public ConfirmationModel(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
+
+		public Booking booking { get; set; }
+		public ConfirmTicket[] tickets = new ConfirmTicket[] { };
+        public void OnGet(int id)
+		{
+
+			booking = _unitOfWork.BookingRepo.Get(id);
+			ConfirmTicket confirmTicket;
+
+			foreach (var ticket in _unitOfWork.TicketRepo.GetAll())
 			{
-				_unitOfWork = unitOfWork;
-			}
-
-			public Booking booking { get; set; }
-
-			public void OnGet(int id)
-			{
-
-				booking = _unitOfWork.BookingRepo.Get(id);
-				
-				foreach (var ticket in _unitOfWork.TicketRepo.GetAll())
+				if (ticket.BookingId == booking.Id)
 				{
-					if (ticket.BookingId == booking.Id)
-					{
-						ticket.TicketType = _unitOfWork.TicketTypeRepo.Get(ticket.TicketTypeId);
-						ticket.Screening = _unitOfWork.ScreeningRepo.Get(ticket.ScreeningId);
-						ticket.Screening.Film = _unitOfWork.FilmRepo.Get(ticket.Screening.FilmID);
+					ticket.TicketType = _unitOfWork.TicketTypeRepo.Get(ticket.TicketTypeId);
+					ticket.Screening = _unitOfWork.ScreeningRepo.Get(ticket.ScreeningId);
+					ticket.Screening.Film = _unitOfWork.FilmRepo.Get(ticket.Screening.FilmID);
 						
-					}
-				
 				}
-
+				
 			}
 
-	}
+			foreach (var type in _unitOfWork.TicketTypeRepo.GetAll())
+			{
+				confirmTicket = new ConfirmTicket();
+				confirmTicket.Count = _unitOfWork.TicketRepo.GetAll().Where(t => t.TicketType == type && t.BookingId == id).Count();
+				confirmTicket.Type = type;
+				confirmTicket.TotalPrice = type.Price * confirmTicket.Count;
+
+                tickets.Append(confirmTicket);
+
+
+            }
+
+			QRCodeGenerator qrGenerator = new QRCodeGenerator();
+			QRCodeData qrCodeData = qrGenerator.CreateQrCode("The text which should be encoded.", QRCodeGenerator.ECCLevel.Q);
+			SvgQRCode qrCode = new SvgQRCode(qrCodeData);
+			string qrCodeAsSvg = qrCode.GetGraphic(20);
+
+        }
+
+    }
 }
---
+++ CinemaProject/Pages/PageViewModels/ConfirmTicket.cs
@@ -0,0 +1,11 @@
+﻿using CinemaProject.Models.Models;
+
+namespace CinemaProject.Pages.PageViewModels
+{
+    public class ConfirmTicket
+    {
+        public int Count { get; set; }
+        public double TotalPrice { get; set; }
+        public TicketType Type { get; set; }
+    }
+}
